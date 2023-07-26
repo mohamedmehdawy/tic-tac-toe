f0cc352fae5cd724f383d7fdcd4f526fdedc9795
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-07-26 18:58:13 +0300

    handel share data between parent and children

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,32 @@
-'use client'
+'use client';
 import { useState } from "react";
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(""));
+
+  function handelClick(i: number) {
+    const newArray = squares.slice();
+
+    newArray[i] = "X";
+
+    setSquares(newArray);
+  }
   return (
     <>
       <section className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={() => handelClick(0)}/>
+        <Square value={squares[1]} onSquareClick={() => handelClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handelClick(2)}/>
 
       </section>
       <section className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handelClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handelClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handelClick(5)}/>
       </section>
       <section className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handelClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handelClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handelClick(8)}/>
       </section>
     </>
 
@@ -25,12 +34,8 @@ export default function Board() {
   )
 }
 
-function Square() {
-  const [value, setValue] = useState("");
+function Square({ value, onSquareClick }) {
 
-  function updateValue() {
-    setValue("X");
-  }
-  return <button onClick={updateValue} className="square">{ value }</button>;
+  return <button onClick={onSquareClick} className="square">{ value }</button>;
 
 }
